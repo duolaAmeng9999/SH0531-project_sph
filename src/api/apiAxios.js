d511db651718b6390a722a1d8c626ab9412f60f0
多啦A梦 <86274470+duolaAmeng9999@users.noreply.github.com>
2021-10-10 16:17:46 +0800

    wenjing

---
+++ src/api/apiAxios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const apiAxios = axios.create({
+    baseURL:"/api"
+})
+// 添加请求拦截器
+apiAxios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    return config;
+});
+
+// 添加响应拦截器
+apiAxios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    return response.data;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
+export default apiAxios
